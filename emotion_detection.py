c594992f641ab4ae7c113c2b53e10334c1d38e22
ArchyDuke2025 <ingrafaelrodriguezdiaz@gmail.com>
2024-12-26 17:31:40 -0500

    task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -15,6 +15,18 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } }
 
     # Make a POST request to the API with the payload and headers
-    response = requests.post(url, json=myobj, headers=headers)
+    response = requests.post(url, json = myobj, headers = headers)
 
-    return response.text
+    # Parse the response from the API
+    formatted_response = json.loads(response.text)
+
+    # Extraction of emotions from formatted response
+    emotions_data = formatted_response["emotionPredictions"][0]["emotion"]
+
+    # Find Max value key
+    dominant_emotion = max(emotions_data, key = emotions_data.get)
+
+    #add dominant emotion into emotions_data dictionary
+    emotions_data['dominant_emotion'] = dominant_emotion
+    
+    return emotions_data
